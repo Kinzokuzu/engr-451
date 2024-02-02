20a307e29457c4f3491f12ec92c070b1a3b2ddca
Saul Cervantes <sauljcervantes2@gmail.com>
2024-02-02 12:00:49 -0800

    trimit() implemented

--- lab1_2024/trimit.m
+++ lab1_2024/trimit.m
@@ -1,2 +1,5 @@
 function z = trimit(x)
 % TRIMIT Remove leading and trailing zeros from sequence x and adjust offset appropriately.
+    trim_idx = find(x.data);
+    z.data = x.data(trim_idx(1):trim_idx(end));
+    z.offset = x.offset + trim_idx(1) - 1; % -1 because of 1-based indexing
