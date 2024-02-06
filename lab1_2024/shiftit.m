0ff7b25e195893b570bcc8914d00ddbc4c6661bc
Kinzokuzu <sauljcervantes2@gmail.com>
2024-02-06 13:47:27 -0800

    Passes all test cases now

--- lab1_2024/shiftit.m
+++ lab1_2024/shiftit.m
@@ -1,11 +1,4 @@
 function y = shiftit(x, n0)
 % SHIFTIT Shift a Matlab sequence structure x by integer amount n0 so that y[n] = x[n - n0]
-    if n0 > 0
-        y.data = [zeros(1, n0), x.data];
-    elseif n0 < 0
-        y.data = [x.data, zeros(1, -n0)];
-    else
-        y.data = x.data;
-    end
-
-    y.offset = x.offset;
+    y.data = x.data;
+    y.offset = x.offset + n0;
